19b1762585dfdc504a572e63202804d5fd4a980d
Daniel Roe <daniel@concision.co.uk>
2020-07-28 11:29:59 +0100

    chore: init

---
+++ components/Component1.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Component1
+  </div>
+</template>
+
+<script>
+import Twilio from "twilio-video";
+export default {
+  data: () => ({
+    Twilio,
+  }),
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div>
+      <Component1 v-if="valid" />
+      <Component2 v-else />
+      <button @click="valid = true">
+        Switch components
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+const Component1 = () => import("~/components/Component1.vue");
+const Component2 = () => import("~/components/Component2.vue");
+export default {
+  components: {
+    Component1,
+    Component2,
+  },
+  data: () => ({
+    valid: false,
+  }),
+};
+</script>
